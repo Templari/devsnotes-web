de67279ecf705c0e8f88fa2f46709dc71c81e2ea
Ivan Filho <ivanfilho21@gmail.com>
2020-09-28 20:20:44 -0300

    Adicionar funcoes e refatorar

--- js/util.js
+++ js/util.js
@@ -1,18 +1,50 @@
-function ajax(url, method, callback, form = false, token = false) {
+function runAjax (form, callback, token = false) {
+  let method = form.method
+  let submit = form.querySelector('*[type=submit]')
+
+  method = method.value ? method.value : method
+  submit.disabled = true
+
+  ajax(form.dataset.url, method, (json, status) => {
+    if (callback(json, status)) {
+      window.location = form.action
+    }
+  }, form, token)
+
+  return false
+}
+
+function ajax(url, method, callback, form = false, token = false, lang = 'pt_BR') {
   let formData = form ? new FormData(form) : null;
 
   let request = new XMLHttpRequest();
   request.overrideMimeType("application/json");
   request.open(method, url, true);
+
   if (token) {
     request.setRequestHeader('Authorization', `Bearer ${token}`)
   }
+
+  request.setRequestHeader('Accept-Language', lang)
+
   request.onreadystatechange = () => {
     if (request.readyState == 4) {
-      callback(request.responseText);
+      let res = []
+
+      try {
+        res = JSON.parse(request.responseText)
+      } catch (e) {
+        console.warn(e)
+      }
+
+      callback(res, request.status)
     }
   };
   request.send(formData);
+}
+
+function deleteCookie(cname) {
+  document.cookie = cname + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
 
 function setCookie(cname, cvalue, exdays) {
